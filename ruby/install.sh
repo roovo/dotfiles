a8d4952c259be0e165ce9119e58f7ee1b905add1
rupert <rupert.voelcker@bt.com>
2015-03-18 13:19:35 +0000

    rvm: ask before installing

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
 set -e
-if [[ ! $(which rvm) ]]; then
-  echo "Installing rvm"
-  \curl -sSL https://get.rvm.io | bash
+
+info () {
+  printf "  [ \033[00;34m..\033[0m ] $1"
+}
+
+user () {
+  printf "\r  [ \033[0;33m?\033[0m ] $1 "
+}
+
+if [[  $(which rvm) ]]; then
+  user ' - rvm not installed - install it? [y/N]'
+  read -n 1 install_rvm
+  echo ''
+
+  if [[ "${install_rvm}" == "y" ]]; then
+    info "Installing rvm\n"
+    \curl -sSL https://get.rvm.io | bash
+  fi
 fi
